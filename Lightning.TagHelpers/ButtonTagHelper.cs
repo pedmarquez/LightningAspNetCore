2a483305d7a3de2a1ba59381e1386386c15f1b35
Pedro Marquez <pedro_marquez@outlook.com>
2019-07-03 12:34:55 -0400

    Add project files.

---
+++ Lightning.TagHelpers/ButtonTagHelper.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Collections.Generic;
+
+namespace Lightning.TagHelpers
+{
+    public enum Variante
+    {
+        None,
+        Neutral,
+        Brand,
+        OutlineBrand,
+        Destructive
+    }
+    [HtmlTargetElement(Attributes = "slds-button")]
+    public class ButtonTagHelper : TagHelper
+    {
+        private Dictionary<Variante, string> buttonStyles = new Dictionary<Variante, string> {
+             { Variante.Neutral, "neutral" },
+             { Variante.Brand, "brand" },
+             { Variante.OutlineBrand, "outline-brand" },
+             { Variante.Destructive, "destructive" }
+        };
+        public Variante Variante { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.RemoveAll("slds-button");
+
+            var clases = new List<string> { "slds-button" };
+
+            if (Variante != Variante.None)
+                clases.Add($"slds-button_{ buttonStyles[this.Variante]}");
+
+            if (output.Attributes.TryGetAttribute("class", out var @class))
+                clases.Add(@class.Value.ToString());
+
+            output.Attributes.SetAttribute("class", string.Join(" ", clases));
+        }
+    }
+}
---
+++ Lightning.TagHelpers/IconTagHelper.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Collections.Generic;
+
+namespace Lightning.TagHelpers
+{
+    public enum IconColor
+    {
+        None,
+        Default,
+        Success,
+        Warning,
+        Error,
+        Light
+    }
+    public enum IconSprite
+    {
+      //  None,
+        Action,
+        Custom,
+        Doctype,
+        Standard,
+        Utility
+    }
+
+
+    [HtmlTargetElement("slds-icon")]
+    public class IconTagHelper: TagHelper
+    {
+        public IconColor IconColor { get; set; }
+        public IconSprite Sprite { get; set; }
+        public string Icon { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var clases = new List<string> { "slds-icon" };
+            output.TagName = "svg";
+          
+
+            if(IconColor != IconColor.None)
+               clases.Add( $"slds-icon-text-{IconColor.ToString().ToLower()}");
+
+            output.Attributes.SetAttribute("class", string.Join(" ", clases));
+
+            output.Content.SetHtmlContent($"<use xlink:href='/assets/icons/{Sprite.ToString().ToLower()}-sprite/svg/symbols.svg#{Icon}'></use>");
+        }
+    }
+}
