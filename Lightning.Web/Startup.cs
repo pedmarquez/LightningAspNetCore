79fb2ea2f697f9ee40e892e51077f0cb4612aad2
Pedro Marquez <pedro.marquez@afexav.cl>
2019-07-03 13:42:25 -0400

    Remove MVC and change to Razor Pages

--- Lightning.Web/Startup.cs
+++ Lightning.Web/Startup.cs
@@ -52,13 +52,13 @@ namespace Lightning.Web
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            app.UseMvc();
+            //app.UseMvc(routes =>
+            //{
+            //    routes.MapRoute(
+            //        name: "default",
+            //        template: "{controller=Home}/{action=Index}/{id?}");
+            //});
         }
     }
 }
